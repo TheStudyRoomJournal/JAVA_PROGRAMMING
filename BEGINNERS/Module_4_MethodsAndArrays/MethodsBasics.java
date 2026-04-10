290312c10a15c2126344ea66b5301e3e2c48ce97
TheStudyRoomJournal <thestudyroomjournal@gmail.com>
2026-04-10 13:11:50 +0530

    Create MethodsBasics.java

---
+++ BEGINNERS/Module_4_MethodsAndArrays/MethodsBasics.java
@@ -0,0 +1,10 @@
+public class MethodsBasics {
+
+    static void greet() {
+        System.out.println("Hello, Java!");
+    }
+
+    public static void main(String[] args) {
+        greet();
+    }
+}
