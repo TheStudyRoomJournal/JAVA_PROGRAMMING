13c87eae1ff158374591a15b4676b6d4785bc3e0
TheStudyRoomJournal <thestudyroomjournal@gmail.com>
2026-04-10 13:11:42 +0530

    Create IfElseElseIf.java

---
+++ BEGINNERS/Module_3_ControlFlow/IfElseElseIf.java
@@ -0,0 +1,13 @@
+public class IfElseElseIf {
+    public static void main(String[] args) {
+        int num = 10;
+
+        if (num > 0) {
+            System.out.println("Positive");
+        } else if (num == 0) {
+            System.out.println("Zero");
+        } else {
+            System.out.println("Negative");
+        }
+    }
+}
